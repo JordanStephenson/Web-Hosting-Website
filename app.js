0cf3bf02cb25ad11a1f7ff2bb919fe34e2736195
Jordan Stephenson <jordan_535@hotmail.com>
2020-02-11 00:35:56 +0000

    Initial commiy

---
+++ app.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById('menu');
+const nav = document.getElementById('nav');
+const exit = document.getElementById('exit');
+
+menu.addEventListener('click', (e) => {
+    nav.classList.toggle('hide-mobile');
+    e.preventDefault();
+});
+exit.addEventListener( 'click', (e) => {
+    nav.classList.add('hide-mobile');
+    e.preventDefault();
+} )
